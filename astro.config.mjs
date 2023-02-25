a9b672b37ac8500667bcf736436ffc3715cff8ba
nobuhikosawai <nobuhiko.sawai@gmail.com>
2023-02-26 06:36:43 +0900

    feat: add code snippet title

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,21 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import AutoImport from 'astro-auto-import';
+import {
+  astroCodeSnippets,
+  codeSnippetAutoImport,
+} from './src/integrations/astro-code-snippets';
 
 export default defineConfig({
   site: 'https://nobuhikosawai.com/',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [
+    AutoImport({
+      imports: [codeSnippetAutoImport],
+    }),
+    astroCodeSnippets(),
+    sitemap(),
+    tailwind(),
+    mdx(),
+  ],
 });
