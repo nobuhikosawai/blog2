49d09000b245d33f24dbec7353a0b9f30e9a41a0
nobuhikosawai <nobuhiko.sawai@gmail.com>
2023-02-27 15:19:31 +0900

    feat: add dark theme

---
+++ public/theme-toggle.js
@@ -0,0 +1,63 @@
+const THEME_KEY = 'dark-mode-toggle';
+
+/**
+ * Get theme
+ * @return {'dark' | 'light'}
+ */
+function getTheme() {
+  const theme = localStorage.getItem(THEME_KEY);
+
+  return (
+    theme ??
+    (window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'dark'
+      : 'light')
+  );
+}
+
+/**
+ * Apply preference
+ * @param {'dark' | 'light'}
+ */
+function setPreference(theme) {
+  const html = document.documentElement;
+  // html.classList.add(theme);
+  html.dataset.theme = theme; //['data-theme'] = theme;
+
+  const button = document.getElementById('theme-toggle');
+  button?.setAttribute('aria-label', theme);
+  if (theme === 'dark') {
+    button?.classList.add('theme-toggle--toggled');
+  } else {
+    button?.classList.remove('theme-toggle--toggled');
+  }
+}
+
+/**
+ * Save preference on localStorage
+ * @param {'dark' | 'light'}
+ */
+function savePreference(theme) {
+  localStorage.setItem(THEME_KEY, theme);
+}
+
+// Calling this as fast as possible to avoid the flash
+// https://web.dev/building-a-theme-switch-component/#javascript
+// https://github.com/donavon/use-dark-mode/blob/develop/README.md#that-flash
+setPreference(getTheme());
+
+window.onload = () => {
+  const theme = getTheme();
+  setPreference(getTheme());
+
+  document.getElementById('theme-toggle').addEventListener('click', () => {
+    const t = getTheme() === 'dark' ? 'light' : 'dark';
+    savePreference(t);
+    setPreference(t);
+  });
+};
+
+window.addEventListener('colorscheme', (event) => {
+  const theme = event.detail.colorScheme;
+  setPreference(theme);
+});
