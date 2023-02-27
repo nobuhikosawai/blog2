d2bf9654e199819df6af888927b3bd3b14cc71b2
nobuhikosawai <nobuhiko.sawai@gmail.com>
2023-02-28 04:48:25 +0900

    feat: accent with anchor ilnk

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,47 @@
 /** @type {import('tailwindcss').Config} */
+
+const lightAnchor = (theme) => {
+  return {
+    color: theme('colors.fuchsia.700'),
+    textDecoration: 'underline',
+  };
+};
+
+const darkAnchor = (theme) => {
+  return {
+    color: theme('colors.fuchsia.300'),
+  };
+};
+
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      typography: (theme) => {
+        const lightTheme = lightAnchor(theme);
+        return {
+          DEFAULT: {
+            css: {
+              a: lightTheme,
+            },
+          },
+        };
+      },
+    },
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [
+    require('@tailwindcss/typography'),
+    function ({ addBase, theme }) {
+      const lightTheme = lightAnchor(theme);
+      const darkTheme = darkAnchor(theme);
+
+      addBase({
+        a: lightTheme,
+        'html[data-theme = "dark"]': {
+          a: darkTheme,
+        },
+      });
+    },
+  ],
   darkMode: ['class', '[data-theme="dark"]'],
 };
