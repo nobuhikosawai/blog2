75eaece233b2c77526c635377ca741f15763f0f9
nobuhikosawai <nobuhiko.sawai@gmail.com>
2023-03-01 01:01:38 +0900

    feat: automatically convert embedded content

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,12 +9,38 @@ import {
 } from './src/integrations/astro-code-snippets';
 import partytown from '@astrojs/partytown';
 
+import remarkEmbedder from '@remark-embedder/core';
+import oembedTransformer from '@remark-embedder/transformer-oembed';
+
 // https://astro.build/config
 import image from '@astrojs/image';
+
+// for oembedTransformer
+function handleHTML(html, info) {
+  const { url, transformer } = info;
+  if (
+    transformer.name === '@remark-embedder/transformer-oembed' ||
+    url.includes('twitter.com')
+  ) {
+    return `<div style="max-width: max-content; margin-left: auto; margin-right: auto">${html}</div>`;
+  }
+  return html;
+}
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nobuhikosawai.com/',
+  markdown: {
+    remarkPlugins: [
+      [
+        remarkEmbedder,
+        {
+          transformers: [oembedTransformer],
+          handleHTML,
+        },
+      ],
+    ],
+  },
   integrations: [
     AutoImport({
       imports: [codeSnippetAutoImport],
