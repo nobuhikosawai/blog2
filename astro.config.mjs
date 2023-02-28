a224e32aa3837780883db04181c303f3860bef2c
nobuhikosawai <nobuhiko.sawai@gmail.com>
2023-02-28 14:13:20 +0900

    feat: add GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,17 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import AutoImport from 'astro-auto-import';
-import {
-  astroCodeSnippets,
-  codeSnippetAutoImport,
-} from './src/integrations/astro-code-snippets';
+import { astroCodeSnippets, codeSnippetAutoImport } from './src/integrations/astro-code-snippets';
+import partytown from "@astrojs/partytown";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://nobuhikosawai.com/',
-  integrations: [
-    AutoImport({
-      imports: [codeSnippetAutoImport],
-    }),
-    astroCodeSnippets(),
-    sitemap(),
-    tailwind(),
-    mdx(),
-  ],
+  integrations: [AutoImport({
+    imports: [codeSnippetAutoImport]
+  }), astroCodeSnippets(), sitemap(), tailwind(), mdx(), partytown({
+    config: { 
+      forward: ["dataLayer.push"] 
+    },
+    })]
 });
